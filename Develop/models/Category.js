6902b2391ea3adf9c33ce5537f3f89f8fba4fcc4
Jesse Orozco <orozcojesse0@gmail.com>
2021-05-10 18:53:27 -0400

    added properties for Category.js file

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,19 +1,28 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    location_name: {
+      type: DataTypes.STRING,
+      allowNull: False,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
