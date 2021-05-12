753a9774162855df3e13a099f2560980252ca018
Jesse Orozco <orozcojesse0@gmail.com>
2021-05-12 18:08:42 -0400

    redue index.js

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,3 @@ router.use((req, res) => {
 });
 
 module.exports = router;
-const router = require("express").Router();
-const apiRoutes = require("./api");
-
-router.use("/api", apiRoutes);
-
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>");
-});
-
-module.exports = router;
